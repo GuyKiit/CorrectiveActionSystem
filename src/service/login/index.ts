d201ff1448535c7421ae84b8c3ccc00831c51dcb
Willy ''Winz'' Sirirungsakulwong <ispeedywilly@gmail.com>
2025-12-29 13:42:16 +0700

    Update : PROD + UAT ENV Files, Change Url Path on Function [login_auth_emp_get]

--- src/service/login/index.ts
+++ src/service/login/index.ts
@@ -3,8 +3,8 @@ import { AuthModel, logintType } from "../../types/users";
 
 // Server should return AuthModel
 export function login_auth_emp_get(element: logintType) {
-    const url = `${import.meta.env.VITE_APP_API_URL}/api_sys_auth/SysAuth/login_auth_emp_get`
-    return axios.post<AuthModel>(url, 
-      element
-    );
-  }
+  const url = `${import.meta.env.VITE_APP_API_URL_EMP_LOGIN}/api_sys_auth/SysAuth/login_auth_emp_get`
+  return axios.post<AuthModel>(url,
+    element
+  );
+}
