cdb4851bc1d88da9618f97f8d374991f00f76803
Willy ''Winz'' Sirirungsakulwong <ispeedywilly@gmail.com>
2026-01-30 14:17:00 +0700

    Update : Add Home Page Wallpaper

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,9 +5,9 @@ export default function Home() {
       <div className="max-lg rounded overflow-hidden bg-gray-200" style={{ marginTop: '4rem' }}>
         <div className="col-md-12"
           style={{
-            // backgroundImage: 'url(media/slider/home-bg.jpg)',
-            // backgroundSize: 'cover',
-            // backgroundPosition: 'center',
+            backgroundImage: 'url(media/slider/cas-home-bg.png)',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
             width: '100%',
             height: '600px',
             display: 'flex',
